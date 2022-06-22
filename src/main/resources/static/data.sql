9dc0192a600c68d4c4beab816a633d6007dbc52e
Julia Mersing Ortiz <mariajulia.mersing@clearpay.com>
2022-06-22 16:51:26 +0200

    feat: add sql.data

---
+++ src/main/resources/static/data.sql
@@ -0,0 +1,16 @@
+insert into filmDirector.film_director (id, biography, name) values (1,'Skipping ahead several decades, Agnés Varda´s films "Le Bonheur", "Cléo from 5 to 7" and "Vagabond" often show up on lists of essential films from female directors, and for good reason.
+Varda´s work manages to employ realism while experimenting with both form and genre. It also has strong social messages.
+Because of Varda´s background in photography, she established a distinct visual style and often uses still images in her storytelling.
+She was the first woman to receive an honorary Palme d´Or at the Cannes Film Festival, and to this day, she is the only woman to receive an honorary Oscar for directing.
+At the Telluride Film Festival in 2019, a few months after Varda passed away, Martin Scorcese called her a "God of Cinema"' ,'Agnès Varda');
+insert into filmDirector.film_director (id, biography, name) values (2, 'Céline Sciamma is a French screenwriter and film director.
+She is especially known for her films Girlhood, My Life as a Courgette,
+and Portrait of a Lady on Fire, winning many nominations and awards for her films. ','Céline Sciamma');
+insert into filmDirector.film_director (id, biography, name)
+values (3, 'She was a pioneer feminist, film writer, director and actress born in Buenos Aires, Argentina.
+One of the first Latin-American women film directors and a powerful presence in the intellectual Argentina of the 1970-1990.
+In her work, she specialized in portraying famous South American women and the Argentine upper class.
+Her last film was De eso no se habla (1993), starring Marcello Mastroianni. She received Konex Awards in 1984 and 1991 and the Honour Konex in 2001, and multiple awards in international film festivals. She also participated as a jury at the festivals of Cartagena, Berlin, Chicago and Venice.
+At the end of her life, Bemberg was working on a script, based on the story El impostor by Silvina Ocampo (a distant relative) which was made into a film in 1997 directed by her longtime collaborator Alejandro Maci.', 'María Luisa Bemberg');
+insert into filmDirector.film_director (id, biography, name) values (4, 'Janet Mock is a writer, director and executive producer for the FX series "Pose" and the Netflix limited series "Hollywood." She´s also the New York Times bestselling and trailblazing author of two memoirs, Redefining Realness (2014) and Surpassing Certainty (2017) about her journey as a trans woman.
+Janet has received Harvard University´s Artist of the Year Award in 2019 and was named on of The Hollywood Reporter´s "Women in Entertainment Power 100" and included on Vanity Fair´s "New Establishment" list -- adding to her Peabody Award, Television Academy Honors, two AFI Awards and Emmy and Golden Globe nominations for "Pose."', 'Janet Mock')
